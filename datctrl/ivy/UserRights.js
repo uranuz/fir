9a2debfd1de3b10a3745326707e15f51e79ea490
Uranuz <neuranuz@gmail.com>
2018-08-12 10:35:59 +0300

    Added fake IvyUserRights

---
+++ datctrl/ivy/UserRights.js
@@ -0,0 +1,54 @@
+define('fir/datctrl/ivy/UserRights', [
+	'ivy/ClassNode'
+], function(ClassNode) {
+	function IvyUserRights() {
+		this._accessObject = null;
+		this._accessKind = null;
+		this._data = null;
+	};
+	__extends(IvyUserRights, ClassNode);
+	return __mixinProto(IvyUserRights, {
+		/** Analogue to IDataNodeRange opSlice(); in D impl */
+		range: function() {
+			throw new Error('Not implemented!');
+		},
+		/** Analogue to IClassNode opSlice(size_t, size_t); in D impl */
+		slice: function(start, end) {
+			throw new Error('Not implemented!');
+		},
+		/** Analogue to:
+		 * TDataNode opIndex(string);
+		 * TDataNode opIndex(size_t);
+		 * in D impl */
+		at: function(index) {
+			throw new Error('Not implemented!');
+		},
+		/** Analogue to TDataNode __getAttr__(string); in D impl */
+		getAttr: function(name) {
+			switch( name ) {
+				case 'object': return this._accessObject;
+				case 'kind': return this._accessKind;
+				case 'data': return this._data;
+				case 'hasRight': return true; // TODO: Do actual rights check
+				default: throw new Error('Unexpected IvyUserRights attribute: ' + name);
+			}
+		},
+		/** Analogue to void __setAttr__(TDataNode, string); in D impl */
+		setAttr: function(value, name) {
+			switch( name ) {
+				case 'object': this._accessObject = value; break;
+				case 'kind': this._accessKind = value; break;
+				case 'data': this._data = value; break;
+				default: throw new Error('Unexpected IvyUserRights attribute: ' + name);
+			}
+		},
+		/** Analogue to TDataNode __serialize__(); in D impl */
+		serialize: function() {
+			throw new Error('Not implemented!');
+		},
+		/** Analogue to size_t length() @property; in D impl */
+		getLength: function() {
+			throw new Error('Not implemented!');
+		}
+	});
+});
