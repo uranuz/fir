e91434f23fd147343abac4be5f133bbdebac9ac2
Uranuz <neuranuz@gmail.com>
2018-10-19 08:50:02 +0300

    Contol modules init separated into ControlManagement from FirControl

---
+++ controls/ControlManager.js
@@ -0,0 +1,249 @@
+define('fir/controls/ControlManager', [
+	'fir/common/globals',
+	'fir/common/base64',
+	'fir/common/text_encoder',
+], function(globals, Base64, TextEncoder) {
+	function ControlLoadState() {
+		this.controlTag = null; // This control (or area) root tag
+		this.configTag = null; // Configuration tag 'data-fir-opts' inside control tag
+		this.opts = null; // Parsed options
+		this.moduleName = null; // Path to module of this control
+		this.control = null; // This control instance
+		this.childControlTags = null; // Root tags of child controls
+		this.childControls = null; // Child control instances
+		this.childLoadCounter = null; // Count of remaining child controls that need to be loaded
+		this.parentState = null; // Instance of ControlLoadState for parent control
+		this.replaceMarkup = null;
+		this.areaName = null;
+	}
+
+	var Manager = __mixinProto(function ControlManager() {
+		this._controlRegistry = {};
+		this._controlCounter = 0; // Variable used to generate control names
+	}, {
+		/**
+		 * Получает осуществляет "рекурсивный" поиск узлов по списку изначальных узлов roots (по селектору).
+		 * Выибирает только "наименее вложенные" элементы подходящие по селектору.
+		 * Т.е. если внутри подходящего элемента есть еще подходящие, то они уже не будут выбраны.
+		 * Элементы из корневого списка (если подходят под селектор) тоже попадут в результат.
+		 */
+		_getOuterMost: function(roots, selector, stopSelector) {
+			var
+				jRoots = $(roots),
+				nextRoots = [],
+				result = [],
+				it;
+			// Ищем пока есть, где искать
+			while( jRoots.length > 0 ) {
+				// Проход по всем элементам данного уровня
+				for( var i = 0; i < jRoots.length; ++i ) {
+					it = jRoots[i];
+					if( it.nodeType !== 1 ) {
+						continue;
+					}
+					if( $(it).is(selector) ) {
+						result.push(it); // Подошло - добавляем в результат
+					} else {
+						// Не подошло - будем смотреть детей внутри него
+						for( var k = 0; k < it.children.length; ++k ) {
+							var child = it.children[k];
+							// Не заходим внутрь, если поймали критерий останова
+							if( !stopSelector || !$(child).is(stopSelector) ) {
+								nextRoots.push(child);
+							}
+						}
+					}
+				}
+				jRoots = nextRoots;
+				nextRoots = [];
+			}
+			return $(result);
+		},
+
+		/** Запускает компоненты в работу после загрузки или обновления верстки */
+		launchMarkup: function(newMarkup, state) {
+			var self = this;
+			if( state == null ) {
+				// Выбираем корневые компоненты
+				this._getOuterMost(newMarkup, '[data-fir-module]').each(function(index, controlTag) {
+					var newState = new ControlLoadState();
+					newState.controlTag = $(controlTag);
+					self._initControlAndChildren(newState);
+				});
+			} else {
+				state.controlTag = this._getOuterMost(newMarkup, '.' + state.instanceHTMLClass());
+				if( !state.controlTag || state.controlTag.length !== 1 ) {
+					throw new Error(
+						'Expected exactly 1 element as control tag!!! '
+						+ 'Check if matching instance class is present on control root element '
+						+ 'and there is no multiple root control elements in markup.');
+				}
+				self._initControlAndChildren(state);
+			}
+		},
+
+		_onControlModuleLoaded: function(state, ControlClass) {
+			var
+				parentState = state.parentState,
+				updateControl = true;
+			// К этому моменту дочерние компоненты уже загрузились
+			if( state.control == null ) {
+				state.opts.container = $(state.controlTag); // Устанавливаем корневой тэг для компонента
+				state.opts.childControls = state.childControls; // Прописываем контролы всей пачкой (**)
+				state.control = new ControlClass(state.opts);
+				state.control._subscribeInternal();
+				updateControl = false;
+			}
+
+			if( parentState != null ) {
+				// Добавляем наш компонент в список дочерних для родителя
+				// Если компонент - корневой, то parentState == null
+				if( parentState.control == null ) {
+					// Родительский контрол не указан - добавляем просто в список, который потом добавляется в строке (**)
+					parentState.childControls.push(state.control);
+				} else {
+					// Есть родительский контрол - добавляем ещё не существующие дочерние к нему
+					var existingInst = parentState.control.getChildInstanceByName(state.opts.instanceName);
+					if( !existingInst ) {
+						parentState.control._childControls.push(existingInst);
+					}
+				}
+			}
+
+			if( updateControl ) {
+				// Выполняем обновление состояния, когда уже добавили к родителю (на всякий случай)
+				if (!state.areaName) {
+					// Если название области для обновления не указано, то обновлялась вся вёрстка компонента,
+					// поэтому прописываем новый корневой тег в _container
+					state.control._container = $(state.controlTag);
+				}
+				state.control._updateControlState(state.opts);
+				state.control._subscribeInternal();
+			}
+
+			state.control._onAfterLoad();
+
+			if( parentState != null && --(parentState.childLoadCounter) === 0 ) {
+				// Все компоненты родителя подгрузились - инициализируем его
+				this._initCurrentControl(parentState);
+			}
+		},
+
+		_extractControlOpts: function(rawOpts) {
+			var decodedOpts;
+			if( rawOpts ) {
+				decodedOpts = Base64.decodeUTF8(rawOpts);
+				return JSON.parse(decodedOpts);
+			}
+			return {};
+		},
+
+		_initCurrentControl: function(state) {
+			require([state.moduleName], this._onControlModuleLoaded.bind(this, state));
+		},
+
+		_fillControlStateFromMarkup: function(state) {
+			if( state.moduleName ) {
+				return state;
+			}
+			state.configTag = this._getOuterMost($(state.controlTag), '[data-fir-opts]', '[data-fir-module]');
+			if( !state.controlTag || state.controlTag.length !== 1 ) {
+				throw new Error('Expected exactly 1 element as control tag!!!');
+			}
+			if( state.configTag.length > 1 ) {
+				throw new Error('Multiple "opts" tags found for control!!!');
+			}
+			state.moduleName = $(state.controlTag).attr('data-fir-module');
+			try {
+				state.opts = this._extractControlOpts(state.configTag.attr('data-fir-opts'));
+			} catch (ex) {
+				throw new Error('Failed to parse control options from markup!!!');
+			}
+			state.childControlTags = this._getOuterMost(state.controlTag.children(), '[data-fir-module]');
+			state.childLoadCounter = state.childControlTags.length;
+			if( state.control == null ) {
+				state.childControls = []; // Список дочерних для текущего компонента
+			}
+			return state;
+		},
+		/** Обновление вёрстки компонента при его перезагрузке */
+		_updateControlMarkup: function(state) {
+			// Замена старой верстки компонента на новую
+			// TODO: Нужно проверить, что это не дочерний компонент внутри новой вёрстки,
+			// которую не нужно заменять, поскольку она уже будет заменена
+			if( state.control._container.length && state.controlTag.length ) {
+				// Обходим баг в jQuery  replaceWith через родной replaceChild
+				// Проблема в том, что jQuery вместо родителя заменяемого элемента берет родителя передаваемого на замену
+				$.cleanData(state.control._container); // Почистим jQuery данные старого контейнера, т.к. это делает jQuery
+				state.control._container[0].parentNode.replaceChild(state.controlTag[0], state.control._container[0]);
+			}
+		},
+
+		_initControlAndChildren: function(state) {
+			var
+				self = this,
+				childStates = [],
+				childrenExist = {};
+			this._fillControlStateFromMarkup(state);
+			state.childControlTags.each(function(index, childTag) {
+				var childState = new ControlLoadState();
+				childState.controlTag = $(childTag);
+				childState.parentState = state;
+				self._fillControlStateFromMarkup(childState);
+				if( state.control ) {
+					// Если дочерний контрол уже существует в текущем,
+					// то сохраняем ссылку на него, чтобы он не был создан заново
+					childState.control = state.control.getChildInstanceByName(childState.opts.instanceName);
+				}
+				childStates.push(childState);
+				childrenExist[childState.opts.instanceName] = true;
+			});
+			if( state.control ) {
+				var childControls = state.control._childControls;
+				// Лучше будем удалять с конца, чтобы не было проблем с возможным смещением индексов
+				for( var j = childControls.length - 1; j > 0; --j ) {
+					if( !childrenExist[ childControls[j] ] ) {
+						// При перезагрузке компонентов вызываем уничтожение тех компонентов в старой верстке,
+						// которых нет в новой верстке.
+						childControls[j].destroy();
+						childControls.splice(j, 1); // Нужно убрать пустые элементы из массива дочерних
+					}
+				}
+			}
+			if( state.control && state.replaceMarkup ) {
+				this._updateControlMarkup(state);
+			}
+			if( state.childLoadCounter === 0 ) {
+				// Нет дочерних компонентов - инициализируем сразу, иначе асинхронно
+				this._initCurrentControl(state);
+			}
+			// require нужен, т.к. при использовании сборок дочерние модули часто находятся в родителе,
+			// и нужно подгрузить родителя первым
+			require([state.moduleName], function() {
+				for( var i = 0; i < childStates.length; ++i ) {
+					self._initControlAndChildren(childStates[i]); // Инициализируем дочерние компоненты
+				}
+			});
+		},
+		registerControl: function(control) {
+			if( this._controlRegistry[control.instanceName()] ) {
+				throw Error('Control with name "' + control.instanceName() + '" already registered!');
+			}
+			this._controlRegistry[control.instanceName()] = control;
+		},
+		unregisterControl: function(control) {
+			// Дерегистрировать компонент из реестра
+			if( this._controlRegistry[control.instanceName()] === control ) {
+				delete this._controlRegistry[control.instanceName()];
+			}
+		},
+		findInstanceByName: function(instanceName) {
+			return this._controlRegistry[instanceName];
+		},
+		getNextNameIndex: function() {
+			return ++this._controlCounter;
+		}
+	});
+
+	return new Manager();
+});
--- controls/FirControl.js
+++ controls/FirControl.js
@@ -1,37 +1,21 @@
 define('fir/controls/FirControl', [
+	'fir/controls/ControlManager',
 	'fir/common/globals',
 	'fir/common/base64',
 	'fir/common/text_encoder',
-], function(globals, Base64, TextEncoder) {
-	var
-		controlCounter = 0, // Variable used to generate control names
-		controlRegistry = {};
-
-	function ControlLoadState() {
-		this.controlTag = null; // This control (or area) root tag
-		this.configTag = null; // Configuration tag 'data-fir-opts' inside control tag
-		this.opts = null; // Parsed options
-		this.moduleName = null; // Path to module of this control
-		this.control = null; // This control instance
-		this.childControlTags = null; // Root tags of child controls
-		this.childControls = null; // Child control instances
-		this.childLoadCounter = null; // Count of remaining child controls that need to be loaded
-		this.parentState = null; // Instance of ControlLoadState for parent control
-		this.replaceMarkup = null;
-		this.areaName = null;
-	}
+], function(ControlManager, globals, Base64, TextEncoder) {
 	return __mixinProto(function FirControl(opts) {
 		if (opts.instanceName) {
 			this._instanceName = opts.instanceName;
 		} else {
-			this._instanceName = 'firControl' + ++controlCounter;
+			this._instanceName = 'firControl' + ControlManager.getNextNameIndex();
 		}
 
 		this._container = opts.container;
 		this._childControls = opts.childControls;
 		this._cssBaseClass = opts.cssBaseClass;
 		this._cssClass = opts.cssClass;
-		this._registerControl(this); // Компонент сам себя регистрирует
+		ControlManager.registerControl(this); // Компонент сам себя регистрирует
 	}, {
 		/// Возвращает имя экземпляра компонента интерфейса
 		instanceName: function() {
@@ -95,18 +79,6 @@ define('fir/controls/FirControl', [
 			return this._container.find('.' + this.instanceHTMLClass());
 		},
 
-		initAllControls: function() {
-			var self = this;
-			// Выбираем корневые компоненты
-			$('[data-fir-module]').filter(function(index, tag) {
-				return $(tag).parents('[data-fir-module]').length === 0;
-			})
-			.each(function(index, controlTag) {
-				var state = new ControlLoadState();
-				state.controlTag = $(controlTag);
-				self._initControlAndChildren(state);
-			});
-		},
 		_unsubscribeInternal: function() {},
 		_subscribeInternal: function() {},
 		_getRequestURI: function() {
@@ -118,7 +90,7 @@ define('fir/controls/FirControl', [
 		_getBodyParams: function() {
 			return '';
 		},
-		/** Обработчик обновления внутреннего состояния компонента при завершении перезвгрузки */
+		/** Обработчик обновления внутреннего состояния компонента при завершении перезагрузки */
 		_updateControlState: function(opts) {},
 		/** Обработчик завершения загрузки компонента для переопределения наследниками */
 		_onAfterLoad: function() {
@@ -146,24 +118,11 @@ define('fir/controls/FirControl', [
 			this._unsubscribeInternal();
 			$.ajax(this._getRequestURI(areaName) + (queryParams? '?' + queryParams: ''), {
 				success: function(html) {
-					var
-						newMarkup = $(html),
-						selector = '.' + self.instanceHTMLClass(),
-						controlTag = newMarkup.find(selector).addBack(selector).filter(function(index, childTag) {
-							return $(childTag).parents(newMarkup, selector).length == 0
-						}),
-						state = new ControlLoadState();
-						if( !controlTag || controlTag.length !== 1 ) {
-							throw new Error(
-								'Expected exactly 1 element as control tag!!! '
-								+ 'Check if matching instance class is present on control root element '
-								+ 'and there is no multiple root control elements in markup.');
-						}
-					state.controlTag = controlTag;
+					var state = new ControlLoadState();
 					state.control = self;
 					state.replaceMarkup = true;
 					state.areaName = areaName;
-					self._initControlAndChildren(state);
+					ControlManager.launchMarkup($(html), state);
 				},
 				error: function(error) {
 					console.error(error);
@@ -171,165 +130,8 @@ define('fir/controls/FirControl', [
 			});
 		},
 
-		_onControlModuleLoaded: function(state, ControlClass) {
-			var
-				parentState = state.parentState,
-				updateControl = true;
-			// К этому моменту дочерние компоненты уже загрузились
-			if( state.control == null ) {
-				state.opts.container = $(state.controlTag); // Устанавливаем корневой тэг для компонента
-				state.opts.childControls = state.childControls; // Прописываем контролы всей пачкой (**)
-				state.control = new ControlClass(state.opts);
-				state.control._subscribeInternal();
-				updateControl = false;
-			}
-
-			if( parentState != null ) {
-				// Добавляем наш компонент в список дочерних для родителя
-				// Если компонент - корневой, то parentState == null
-				if( parentState.control == null ) {
-					// Родительский контрол не указан - добавляем просто в список, который потом добавляется в строке (**)
-					parentState.childControls.push(state.control);
-				} else {
-					// Есть родительский контрол - добавляем ещё не существующие дочерние к нему
-					var existingInst = parentState.control.getChildInstanceByName(state.opts.instanceName);
-					if( !existingInst ) {
-						parentState.control._childControls.push(existingInst);
-					}
-				}
-			}
-
-			if( updateControl ) {
-				// Выполняем обновление состояния, когда уже добавили к родителю (на всякий случай)
-				if (!state.areaName) {
-					// Если название области для обновления не указано, то обновлялась вся вёрстка компонента,
-					// поэтому прописываем новый корневой тег в _container
-					state.control._container = $(state.controlTag);
-				}
-				state.control._updateControlState(state.opts);
-				state.control._subscribeInternal();
-			}
-
-			state.control._onAfterLoad();
-
-			if( parentState != null && --(parentState.childLoadCounter) === 0 ) {
-				// Все компоненты родителя подгрузились - инициализируем его
-				this._initCurrentControl(parentState);
-			}
-		},
-
-		_extractControlOpts: function(rawOpts) {
-			var decodedOpts;
-			if( rawOpts ) {
-				decodedOpts = Base64.decodeUTF8(rawOpts);
-				return JSON.parse(decodedOpts);
-			}
-			return {};
-		},
-
-		_initCurrentControl: function(state) {
-			require([state.moduleName], this._onControlModuleLoaded.bind(this, state));
-		},
-
-		_getChildControlTags: function(controlTag) {
-			return $(controlTag).find('[data-fir-module]').filter(function(index, childTag) {
-				// Выбираем элементы для которых ближайшим родителем является controlTag
-				return $(childTag).parent().closest('[data-fir-module]').is(controlTag);
-			});
-		},
-		_fillControlStateFromMarkup: function(state) {
-			if( state.moduleName ) {
-				return state;
-			}
-			state.configTag = $(state.controlTag).find('[data-fir-opts]').filter(function(index, childTag) {
-				return $(childTag).closest('[data-fir-module]').is(state.controlTag);
-			});
-			if( !state.controlTag || state.controlTag.length !== 1 ) {
-				throw new Error('Expected exactly 1 element as control tag!!!');
-			}
-			if( state.configTag.length > 1 ) {
-				throw new Error('Multiple "opts" tags found for control!!!');
-			}
-			state.moduleName = $(state.controlTag).attr('data-fir-module');
-			try {
-				state.opts = this._extractControlOpts(state.configTag.attr('data-fir-opts'));
-			} catch (ex) {
-				throw new Error('Failed to parse control options from markup!!!');
-			}
-			state.childControlTags = this._getChildControlTags(state.controlTag);
-			state.childLoadCounter = state.childControlTags.length;
-			if( state.control == null ) {
-				state.childControls = []; // Список дочерних для текущего компонента
-			}
-			return state;
-		},
-		/** Обновление вёрстки компонента при его перезагрузке */
-		_updateControlMarkup: function(state) {
-			// Замена старой верстки компонента на новую
-			// TODO: Нужно проверить, что это не дочерний компонент внутри новой вёрстки,
-			// которую не нужно заменять, поскольку она уже будет заменена
-			if( state.control._container.length && state.controlTag.length ) {
-				// Обходим баг в jQuery  replaceWith через родной replaceChild
-				// Проблема в том, что jQuery вместо родителя заменяемого элемента берет родителя передаваемого на замену
-				$.cleanData(state.control._container); // Почистим jQuery данные старого контейнера, т.к. это делает jQuery
-				state.control._container[0].parentNode.replaceChild(state.controlTag[0], state.control._container[0]);
-			}
-		},
-
-		_initControlAndChildren: function(state) {
-			var
-				self = this,
-				childStates = [],
-				childrenExist = {};
-			this._fillControlStateFromMarkup(state);
-			state.childControlTags.each(function(index, childTag) {
-				var childState = new ControlLoadState();
-				childState.controlTag = $(childTag);
-				childState.parentState = state;
-				self._fillControlStateFromMarkup(childState);
-				if( state.control ) {
-					// Если дочерний контрол уже существует в текущем,
-					// то сохраняем ссылку на него, чтобы он не был создан заново
-					childState.control = state.control.getChildInstanceByName(childState.opts.instanceName);
-				}
-				childStates.push(childState);
-				childrenExist[childState.opts.instanceName] = true;
-			});
-			if( state.control ) {
-				var childControls = state.control._childControls;
-				// Лучше будем удалять с конца, чтобы не было проблем с возможным смещением индексов
-				for( var j = childControls.length - 1; j > 0; --j ) {
-					if( !childrenExist[ childControls[j] ] ) {
-						// При перезагрузке компонентов вызываем уничтожение тех компонентов в старой верстке,
-						// которых нет в новой верстке.
-						childControls[j].destroy();
-						childControls.splice(j, 1); // Нужно убрать пустые элементы из массива дочерних
-					}
-				}
-			}
-			if( state.control && state.replaceMarkup ) {
-				this._updateControlMarkup(state);
-			}
-			if( state.childLoadCounter === 0 ) {
-				// Нет дочерних компонентов - инициализируем сразу, иначе асинхронно
-				this._initCurrentControl(state);
-			}
-			// require нужен, т.к. при использовании сборок дочерние модули часто находятся в родителе,
-			// и нужно подгрузить родителя первым
-			require([state.moduleName], function() {
-				for( var i = 0; i < childStates.length; ++i ) {
-					self._initControlAndChildren(childStates[i]); // Инициализируем дочерние компоненты
-				}
-			});
-		},
-		_registerControl: function(control) {
-			if( controlRegistry[control.instanceName()] ) {
-				throw Error('Control with name "' + control.instanceName() + '" already registered!');
-			}
-			controlRegistry[control.instanceName()] = control;
-		},
 		findInstanceByName: function(instanceName) {
-			return controlRegistry[instanceName];
+			return ControlManager.findInstanceByName(instanceName);
 		},
 		getChildInstanceByName: function(name) {
 			for( var i = 0; i < this._childControls.length; ++i ) {
@@ -355,9 +157,7 @@ define('fir/controls/FirControl', [
 			}
 
 			// Дерегистрировать компонент из реестра
-			if( controlRegistry[this.instanceName()] === this ) {
-				delete controlRegistry[this.instanceName()];
-			}
+			ControlManager.unregisterControl(this);
 		}
 	});
 });
