89c1630e3f47dd5b4c44bd0834da7e4df199a7e6
Uranuz <neuranuz@gmail.com>
2019-02-22 08:42:00 +0300

    Added form data serialization compatible with webtank to IvyServerRender

--- controls/Loader/IvyServerRender.js
+++ controls/Loader/IvyServerRender.js
@@ -13,12 +13,70 @@ return FirClass(
 			return !opts.method;
 		},
 		load: function(opts) {
-			$.ajax(opts.URI + (opts.HTTPMethod === 'post'? '?' + $.param(opts.queryParams): ''), {
+			for( var key in opts.viewParams ) {
+				if( !opts.viewParams.hasOwnProperty(key) ) {
+					continue;
+				}
+				opts.queryParams[key] = opts.viewParams[key];
+			}
+			var queryParams = this.serialize(opts.queryParams);
+			$.ajax(opts.URI + (queryParams? '?' + queryParams: ''), {
 				success: opts.success,
 				error: opts.error,
 				type: opts.HTTPMethod,
-				data: (opts.HTTPMethod === 'post'? opts.bodyParams: opts.queryParams)
+				data: this.serialize(opts.bodyParams)
 			});
+		},
+		linearizeObj: function(params, res, pre) {
+			res = res || {};
+			pre = pre || '';
+			// TODO: implement data serialization logic
+			if( params === null ) {
+				res[pre] = 'null';
+			} else if( params === true ) {
+				res[pre] = 'true';
+			} else if( params === false ) {
+				res[pre] = 'false'
+			} else if( typeof(params) === 'number' || params instanceof Number ) {
+				res[pre] = String(params);
+			} else if( typeof(params) === 'string' || params instanceof String ) {
+				res[pre] = params;
+			} else if( params instanceof Array ) {
+				res[pre] = JSON.stringify(params);
+			} else if( params instanceof Date ) {
+				res[pre] = params.toISOString();
+			} else if( params instanceof Object ) {
+				for( var key in params ) {
+					if( !params.hasOwnProperty(key) ) {
+						continue;
+					}
+					this.linearizeObj(params[key], res, (pre? pre + '__': '') + key);
+				}
+			} else if( params !== undefined ) {
+				throw new Error('Unexpected thing happened during serialization!');
+			}
+			return res;
+		},
+		serialize: function(params) {
+			var
+				res = '',
+				linear = this.linearizeObj(params);
+			if( !(linear instanceof Object) ) {
+				throw new Error('Expected linearized object');
+			}
+			for( var key in linear ) {
+				if( !linear.hasOwnProperty(key) ) {
+					continue;
+				}
+				var items = (linear[key] instanceof Array)? linear[key]: [linear[key]];
+				for( var i = 0; i < items.length; ++i ) {
+					if( res ) {
+						res += '&';
+					}
+					res += encodeURIComponent(key) + '=' + encodeURIComponent(items[i]);
+				}
+			}
+			return res;
 		}
 	}
 );
